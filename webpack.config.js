f62a1a7d76868f55ea59ec3bda37809a8f5a5609
yakoo292929 <yakoo292929@gmail.com>
2023-06-18 08:54:57 +0900

    Generate HTML through Pug

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,21 @@ module.exports = {
           //   }
           // },
         ],
-      }
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            }
+          }
+        ],
+      },
     ],
   },
   plugins: [
@@ -56,7 +70,12 @@ module.exports = {
       filename: './stylesheets/main.css',
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html',
+      template: './src/templates/index.pug',
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html',
     }),
     new CleanWebpackPlugin(),
   ],
